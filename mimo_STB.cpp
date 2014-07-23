0ed006daec838c33c98715d4018234aaaa803fff
winhucw <tonic@TONIC-MacBook-Air.local>
2014-07-23 13:31:57 +0800

    0723

--- mimo_STB.cpp
+++ mimo_STB.cpp
@@ -30,29 +30,26 @@ int main(int argc,char **argv) {
     const int channel_uses = stc.get_channel_uses();
     const int symb_block = stc.get_nb_symbols_per_block();
     
-    SISO siso;
-    cout << "Check : "chan.bits_per_symbol()(0) << "   " << chan.get_symbols()(0) << "   " << chan.get_bits2symbols()(0) << endl;
-    siso.set_constellation(chan.bits_per_symbol()(0), chan.get_symbols()(0), chan.get_bits2symbols()(0));
-    siso.set_demapper_method("Alamouti_maxlogMAP");
-    siso.set_st_block_code(stc.get_nb_symbols_per_block(), stc.get_1st_gen_matrix(), stc.get_2nd_gen_matrix(), nRx);
-    
-    cout << nTx << " TX antennas, " << nRx << " RX antennas, " << (1 << (2*nC)) << "- QAM " << endl;
+//    cout << nTx << " TX antennas, " << nRx << " RX antennas, " << (1 << (2*nC)) << "- QAM " << endl;
     // -- simulation control parameters --
-    const vec EbN0db = "5:5:20";           // SNR range
-    const int Nbits = 1000;           // number of bits to ever simulate per SNR point
+    const vec EbN0db = "-5:1:5";           // SNR range
+    const int Nbits = 1000000;           // number of bits to ever simulate per SNR point
     
     // -- Channel code parameters --
     Convolutional_Code code;
-    ivec generator(3);
-    generator(0) = 0133;                            // use rate 1/3 code
-    generator(1) = 0165;
-    generator(2) = 0171;
+    ivec generator = "0133 0165 0171";
+    int constraint_len = 7;
     double rate = 1.0 / 3.0;
-    code.set_generator_polynomials(generator, 7);   // The encoder has constraint length 7.
+    code.set_generator_polynomials(generator, constraint_len);
     bvec dummy;
     code.encode_tail(randb(Nbits), dummy);
     const int Nc = length(dummy);      // find out how long the coded blocks are
-    const int Nctx = (int)(2 * nC * ceil(double(Nc) / double(2 * nC)));
+    const int Nctx = (int)(2 * nC * symb_block * ceil(double(Nc) / double(2 * nC * symb_block)));
+    
+    SISO siso;
+    siso.set_constellation(chan.bits_per_symbol()(0), chan.get_symbols()(0), chan.get_bits2symbols()(0));
+    siso.set_demapper_method("Alamouti_maxlogMAP");
+    siso.set_st_block_code(stc.get_nb_symbols_per_block(), stc.get_1st_gen_matrix(), stc.get_2nd_gen_matrix(), nRx);
     
     // -- ofdm modulation --
     const int Nfft = 1024; // 1024
@@ -68,13 +65,13 @@ int main(int argc,char **argv) {
     
     // ================== Run simulation =======================
     for (int nsnr = 0; nsnr < length(EbN0db); nsnr++) {
-        cout << " Run " << nsnr << " time " << endl;
+//        cout << " --------- Run " << nsnr << " time -------- " << endl;
         const double Eb = 1.0;      // transmitted energy per information bit
         const double N0 = inv_dB(-EbN0db(nsnr));
         const double sigma2 = N0;   // Variance of each scalar complex noise sample
         const double Es = rate * 2 * nC * Eb; // Energy per complex scalar symbol
         BERC berc;     // counter for coded BER
-        BERC bercu;
+        BERC bercu;    // counter for uncoded BER
         
         awgn.set_noise(N0);
         siso.set_noise(N0 / 2);
@@ -82,15 +79,13 @@ int main(int argc,char **argv) {
         // -- generate and encode random data --
         bvec inputbits = randb(Nbits);
         bvec txbits = code.encode_tail(inputbits);
-        cout << " Length of bit stream after convolution = " << Nc << endl;
-        
+
         // -- mapping --
-        int Nmap = Nc / (2 * nC);  // length of complex symbols after mapping
         if (Nc != Nctx) {
-            Nmap++;
             txbits = concat(txbits, randb(Nctx - Nc));
         }
-        cout << " Length of complex symbols after mapping = " << Nmap << endl;
+        const int Nmap = Nctx / (2 * nC);  // length of complex symbols after mapping
+//        cout << " Length of complex symbols after mapping = " << Nmap << endl;
         cvec inputsymbol(Nmap);
         for (int k = 0; k < Nmap; k++) {
             bvec bitstmp = txbits(k * (2 * nC), (k + 1) * (2 * nC) - 1);
@@ -99,19 +94,19 @@ int main(int argc,char **argv) {
 //        cout << " inputsymbol \n" << inputsymbol(0, 5) << '\n' << inputsymbol(Nmap - 5, Nmap - 1) << endl;
         
         // -- STC --
-        int Ns = Nmap / symb_block * channel_uses; // length of complex symbols after stc for each antenna
+        const int Ns = Nmap / symb_block * channel_uses; // length of complex symbols after stc for each antenna
         cmat symbol_stc = stc.encode(inputsymbol);
-        cout << " Check Ns = " << Ns << " , symbol_stc.rows() = " << symbol_stc.rows() << " , symbol_stc.cols() = " << symbol_stc.cols() << endl;
+//        cout << " Check Ns = " << Ns << " , symbol_stc.rows() = " << symbol_stc.rows() << " , symbol_stc.cols() = " << symbol_stc.cols() << endl;
         
         // -- IFFT --
+        const int Nf = (Ns / Nfft + 1) * Nfft; // length of complex symbols, multiple of fft
         int add;
         if (Ns % Nfft != 0) {
-            add = (int)(Ns / Nfft + 1) * Nfft - Ns;
+            add = Nf - Ns;
             cvec addzeros = zeros_c(symb_block);
             for (int i = 0; i < add; i++)
                 symbol_stc.append_row(addzeros);
         }
-        int Nf = symbol_stc.rows(); // length of complex symbols, multiple of fft
 //        cout << " symbol_stc lowest rows : \n" << symbol_stc.get_rows(2045,2047) << endl;
 //        cout << " symbol_stc rightest columns \n " << symbol_stc.get_col(1) << endl;
         cmat txsymbol(Nf / Nfft * (Nfft + Ncp), symb_block); // complex transmitted data after ifft and cp; will pass through channel
@@ -123,15 +118,15 @@ int main(int argc,char **argv) {
 //        cout << " txsymbol first row : \n" << txsymbol.get_row(0) << endl;
   
         // -- generate channel and data ----
-        const int tx_duration = txsymbol.rows();  // multiple of (fft + cp)
-        cmat H(nTx * nRx, tx_duration) ;   // channel matrix
+        const int tx_duration = txsymbol.rows();
+        cmat H(nTx * nRx, tx_duration) ;
         H.ones();
         H *= sqrt(Es);
         
         siso.set_impulse_response(H);
-        cmat Y(tx_duration, nRx);            // received data
+        cmat Y(tx_duration, nRx);
         
-        for (int i = 0; i < tx_duration / channel_uses; i++ ) {
+        for (int i = 0; i < tx_duration / channel_uses; i++ ) {  // Y = Hx + n
             Y.set_submatrix(i * channel_uses, 0, txsymbol(i * channel_uses, (i+1) * channel_uses - 1, 0, nTx - 1) * reshape(H.get_col(i), nTx, nRx));
         }
         Y = awgn(Y);
@@ -147,31 +142,37 @@ int main(int argc,char **argv) {
 //        cout << " rxsymbol first row : \n" << rxsymbol.get_row(0) << endl;
         
         vec dem_extridata(Nctx);
-        bvec dem_data(Nc);
         vec dem_apriodata(Nctx);
+        bvec dem_data(Nctx);
         siso.demapper(dem_extridata, rxsymbol, dem_apriodata);
         for (int i = 0; i < Nctx; i++) {
             dem_data(i) = (dem_extridata(i) > 0);
         }
-//        cout << " # dem_extridata = " << dem_extridata.size() << endl;
         
-//        cout << " decode data : \n " << dec_data << " \n # decode data = " << dec_data.size() << endl;
-        cout << "txbits \n" << txbits(0,9) << endl;
-        cout << "dem data \n" << dem_data(0,9) << endl;
-        cout << "Uncoded : # txbits = " << txbits.size() << " , # dem_data = " << dem_data.size() << endl;
+//        cout << "txbits \n" << txbits(0,9) << endl;
+//        cout << "dem data \n" << dem_data(0,9) << endl;
+//        cout << "Uncoded : # txbits = " << txbits.size() << " , # dem_data = " << dem_data.size() << endl;
         bercu.count(txbits, dem_data);
-        cout << " --> BER for uncoded = " << bercu.get_errorrate() << endl;
-        
-        bvec dec_data;
+//        cout << bercu.get_errorrate() << endl;
+
         dem_extridata.del(Nctx - (Nctx - Nc), Nctx - 1);
+        bvec dec_data(Nbits);
         code.decode_tail(dem_extridata, dec_data);
-        
-        cout << "input bits \n" << inputbits(0,9) << endl;
-        cout << "dec bits \n" << dec_data(0,9) << endl;
+        for (int k = 0; k < Nbits; k++) {
+            if (dec_data(k) == 0)
+                dec_data(k) = 1;
+                else
+                dec_data(k) = 0;
+        }
+
+//        cout << "input bits \n" << inputbits(0,9) << endl;
+//        cout << "dec bits \n" << dec_data(0,9) << endl;
         berc.count(inputbits, dec_data);
-        cout << "Coded : # inputbits = " << inputbits.size() << " , # dec_data = " << dec_data.size() << endl;
-        cout << " --> BER for coded = " << berc.get_errorrate() << endl;
+//        cout << "Coded : # inputbits = " << inputbits.size() << " , # dec_data = " << dec_data.size() << endl;
+        cout << berc.get_errorrate() << endl;
         cout.flush();
+        
+        
     }
     return 0;
 }
